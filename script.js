fce3a0362e30e4a0a94cab36a7ebaa971cb6d66b
DCady02 <DJCady@bluevalleyk12.net>
2017-02-27 12:24:17 -0600

    simple issue

--- script.js
+++ script.js
@@ -15,10 +15,10 @@ var main = function() {
     results[2] = row2Parent + column1Parent;
     results[3] = row2Parent + column2Parent;
 
-    $('sol0').html(results[0]);
-    $('sol1').html(results[1]);
-    $('sol2').html(results[2]);
-    $('sol3').html(results[3]);
+    $('#sol0').html(results[0]);
+    $('#sol1').html(results[1]);
+    $('#sol2').html(results[2]);
+    $('#sol3').html(results[3]);
   }
 
   run();
